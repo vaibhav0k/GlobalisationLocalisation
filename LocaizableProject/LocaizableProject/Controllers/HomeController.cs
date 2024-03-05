6ad8e61551988fbc16408926f96c51a70918922e
Vaibhav2002-Tag <72647248+Vaibhav30082002@users.noreply.github.com>
2024-03-05 09:46:09 +0530

    Localisation Project

---
+++ LocaizableProject/LocaizableProject/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+
+using LocaizableProject.Models;
+using Microsoft.AspNetCore.Localization;
+
+namespace LocaizableProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private IHtmlLocalizer<HomeController> _localizer;
+
+    public HomeController(ILogger<HomeController> logger, IHtmlLocalizer<HomeController> localizer)
+    {
+        _logger = logger;
+        _localizer = localizer;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["Welcome"] = _localizer["Welcome"];
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult setLanguage(string culture, string returnUrl)
+    {
+        Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+            new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddYears(1)
+            }
+        );
+
+        return LocalRedirect(returnUrl);
+    }
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ LocaizableProject/LocaizableProject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var cultures = new List<CultureInfo>
+    {
+        new CultureInfo("en"),
+        new CultureInfo("hi"),
+        new CultureInfo("mr"),
+        new CultureInfo("gu"),
+
+    };
+    options.DefaultRequestCulture = new RequestCulture("hi");
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseRequestLocalization(app.Services.CreateScope().ServiceProvider.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
